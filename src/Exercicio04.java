d98a58231ce17e4d84cebd07edec98acbb2e6ac8
guiodrumond <guilhermedrumond1978@gmail.com>
2024-04-17 09:33:37 -0300

    Subindo o projeto

---
+++ src/Exercicio04.java
@@ -0,0 +1,31 @@
+public class Exercicio04 {
+    public static void main(String[] args) {
+
+        String nomeCliente1 = "João";
+        String nomeCliente2 = "Marcela";
+        String nomeCliente3 = "Maria";
+        String nomeCliente4 = "Adalberto";
+        int anoNascimentoCliente1 = 1980;
+        int anoNascimentoCliente2 = 2002;
+        int anoNascimentoCliente3 = 1974;
+        int anoNascimentoCliente4 = 2001;
+
+        System.out.println(">>> Lista dos clientes");
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeCliente1);
+        System.out.println("Ano de nascimento: " + anoNascimentoCliente1);
+
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeCliente2);
+        System.out.println("Ano de nascimento: " + anoNascimentoCliente2);
+
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeCliente3);
+        System.out.println("Ano de nascimento: " + anoNascimentoCliente3);
+
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeCliente4);
+        System.out.println("Ano de nascimento: " + anoNascimentoCliente4);
+
+    }
+}
---
+++ src/Exercicio05.java
@@ -0,0 +1,31 @@
+public class Exercicio05 {
+    public static void main(String[] args) {
+
+        String nomeProduto1 = "Livro";
+        String nomeProduto2 = "Celular";
+        String nomeProduto3 = "Camiseta";
+        String nomeProduto4 = "Tênis";
+        double precoProduto1 = 50.95;
+        double precoProduto2 = 650.00;
+        double precoProduto3 = 54.89;
+        double precoProduto4 = 104.65;
+
+        System.out.println(">>> Lista dos Produtos");
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeProduto1);
+        System.out.println("Preço: R$ " + precoProduto1);
+
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeProduto2);
+        System.out.println("Preço: R$ " + precoProduto2);
+
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeProduto3);
+        System.out.println("Preço: R$ " + precoProduto3);
+
+        System.out.println("________________________________________");
+        System.out.println("Nome: " + nomeProduto4);
+        System.out.println("Preço: R$ " + precoProduto4);
+        
+    }
+}
